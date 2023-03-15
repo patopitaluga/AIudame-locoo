11eab6be6f45ffad6becd41f64881c77f985161c
Patricio Pitaluga <patricio.pitaluga@gmail.com>
2023-03-15 18:38:15 -0300

    Copy answer from terminal to clipboard

--- aiudame-cli.mjs
+++ aiudame-cli.mjs
@@ -1,8 +1,16 @@
 import * as fs from 'fs';
-fs.readFileSync('./.env', 'utf8').split(/[\r\n]+/).forEach((_eachEnv) => {
+import * as path from 'path';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+fs.readFileSync(path.resolve(__dirname, './.env'), 'utf8').split(/[\r\n]+/).forEach((_eachEnv) => {
   const parts = _eachEnv.split('=');
   process.env[parts[0]] = parts[1];
 });
+import { spawn } from 'child_process';
+function pbcopy(data) {
+  var proc = spawn('pbcopy');
+  proc.stdin.write(data); proc.stdin.end();
+}
 
 import { Configuration, OpenAIApi } from 'openai';
 const openai = new OpenAIApi(new Configuration({
@@ -23,12 +31,14 @@ for (let i = 2; i < process.argv.length; i++) {
   argumentsStr += ' ' + process.argv[i];
 }
 
-const prevCache = fs.readFileSync('./cache.json', 'utf8');
+const prevCache = fs.readFileSync(path.resolve(__dirname, './cache.json'), 'utf8');
 const cacheJson = JSON.parse(prevCache);
 const existing = cacheJson.find((_) => _.prompt === argumentsStr);
 if (existing) {
   console.log(existing.anwser);
   console.log('\x1b[0m');
+  pbcopy(existing.anwser);
+  console.log('copied ✔');
 } else {
 openai.createCompletion({
   model: 'text-davinci-003',
@@ -41,8 +51,10 @@ openai.createCompletion({
       prompt: argumentsStr,
       anwser: _.data.choices[0].text.trim(),
     });
-    fs.writeFileSync('cache.json', JSON.stringify(cacheJson, null, 2));
+    fs.writeFileSync(path.resolve(__dirname, './cache.json'), JSON.stringify(cacheJson, null, 2));
     console.log('\x1b[0m');
+    pbcopy(_.data.choices[0].text.trim());
+    console.log('copied ✔');
   })
   .catch((_) => { throw _; });
 }
